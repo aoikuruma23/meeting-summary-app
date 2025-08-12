8894077df5eeaadd1a59e68039bff63a81b7b935
aoikuruma23 <yukihiro3million@gmail.com>
2025-08-12 14:15:05 +0900

    812 1414更新

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -33,7 +33,8 @@ const Login: React.FC = () => {
         return
       }
 
-      const redirectUri = window.location.origin
+      // Googleはimplicit flowでハッシュ(#)にトークンを返すため、専用のコールバックページに戻す
+      const redirectUri = `${window.location.origin}/auth/callback`
       const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=id_token&scope=openid%20email%20profile&nonce=${Date.now()}`
       console.log('DEBUG: Google認証URL:', googleAuthUrl)
       window.location.href = googleAuthUrl
